ccf6f24500a9b72ff65b1f90daeb4784dc6e5395
Nicholas Konzen <nicholaskonzen@gmail.com>
2020-07-30 16:37:16 -0700

    Added functionality to reflect the time of day on the calendar

--- script.js
+++ script.js
@@ -7,9 +7,6 @@ let months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'A
 $(document).ready(function () {
 
     let date = new Date();
-    
-    console.log(`Today is ${days[date.getDay()]}`);
-    
 
     $('#currentDay').text(`Today is ${days[date.getDay()]} ${months[date.getMonth()]} ${date.getDate()}, ${date.getFullYear()}`)
 
@@ -17,17 +14,35 @@ $(document).ready(function () {
 
     $('.description').each(input => {
 
+
+        $('.description')[input].classList.remove('past');
+        $('.description')[input].classList.remove('present');
+        $('.description')[input].classList.remove('future');
+
+        
+        if ($('.description')[input].getAttribute("data-hour") < date.getHours()) {
+            $('.description')[input].classList.add('past')
+        } else if ($('.description')[input].getAttribute("data-hour") == date.getHours()) {
+            $('.description')[input].classList.add('present')
+        } else if ($('.description')[input].getAttribute("data-hour") > date.getHours()) {
+            $('.description')[input].classList.add('future')
+        }
+
         if (localStorage.getItem('descriptions') !== null) {
             $('.description')[input].value = JSON.parse(localStorage.getItem('descriptions'))[input]
         }
 
     })
 
+
+
     // I still need to make the save buttons only save their values, not everything
 
     let save = event => {
         event.preventDefault();
         event.stopPropagation();
+
+        descriptionArray = []
 
         $('.description').each(input => {
             descriptionArray.push($('.description')[input].value)
