2db2ae1230fd7e0520a1421c05b5eaefb8d9342c
Nicholas Konzen <nicholaskonzen@gmail.com>
2020-07-29 17:10:37 -0700

    Completed basic working javscript

--- script.js
+++ script.js
@@ -1,5 +1,38 @@
-$(document).ready(function() {
+let descriptionArray = [];
 
-    
+$(document).ready(function () {
+
+    $('.description').each(input => {
+
+        if (localStorage.getItem('descriptions') !== null) {
+            $('.description')[input].value = JSON.parse(localStorage.getItem('descriptions'))[input]
+        }
+
+    })
+
+    // I still need to make the save buttons only save their values, not everything
+
+    let save = event => {
+        event.preventDefault();
+        event.stopPropagation();
+
+        $('.description').each(input => {
+            descriptionArray.push($('.description')[input].value)
+        })
+
+        localStorage.setItem('descriptions', '')
+
+        localStorage.setItem('descriptions', JSON.stringify(descriptionArray))
+    }
+
+    $('.saveBtn').click(save);
+
+    $('.description').keypress(function(event) {
+        if (event.key === 'Enter') {
+            console.log(event.key);
+            save(event);
+        }
+    })
+
 
 });
